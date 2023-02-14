b290c497e9392f4dbab0769c24da936b146a9e62
MaionePietro <pietromaionespam@gmail.com>
2023-02-14 16:29:56 +0100

    FEAT object team with collaborator

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+const team =[
+    collaborator,
+    {
+        firstName: 'Wayne',
+		lastName: 'Barnett',
+        employment: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        firstName: 'Angela',
+		lastName: 'Caroll',
+        employment: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        firstName: 'Walter',
+		lastName: 'Gordon',
+        employment: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        firstName: 'Angela',
+		lastName: 'Lopez',
+        employment: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        firstName: 'Scott',
+		lastName: 'Estrada',
+        employment: 'Developer',
+        img: 'scott-estrada-developer.jpg',
+    },
+    {
+        firstName: 'Barbara',
+		lastName: 'Ramos',
+        employment: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
